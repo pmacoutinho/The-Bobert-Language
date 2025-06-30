ed62f89ae6f92770d7c77a0607a20da25fcf7488
pmacoutinho <pmacoutinho@ua.pt>
2025-06-30 19:54:28 +0100

    Refactoring

--- tests/lexerTest.c
+++ tests/lexerTest.c
@@ -8,19 +8,21 @@ extern Token get_next_token();
 
 int main() {
     /*
+        extern other
+
         math = {
             x = 3 + 42 * (s - 7)
             y = 4 / [1, 2, 3]
         }
     */
-    source = "extern other\nmath = {\nx = 3 + 42 * (s - 7)\ny = 4 / [1, 2, 3]\n}";
+    source = "extern other\n\nmath = {\nx = 3 + 42 * (s - 7)\ny = 4 / [1, 2, 3]\n}";
 
     Token token;
     do {
         token = get_next_token();
         printf("Token: %-15s Lexeme: '%s' Line: %d\n",
             (char*[]){
-                "EOF", "INT", "IDENTIFIER", "PLUS", "MINUS", 
+                "EOF", "NUMBER", "IDENTIFIER", "PLUS", "MINUS", 
                 "STAR", "SLASH", "ASSIGN", "LPAREN", "RPAREN", 
                 "LCURLY", "RCURLY", "LBRACKET", "RBRACKET", 
                 "COMA", "EXTERN", "UNKNOWN"
